e92ebac51e80017364068b71e7734ee8dbe41796
Pierre <pierre@dash.org>
2018-07-24 09:28:20 +0200

    orphans

--- test/index.test.js
+++ test/index.test.js
@@ -22,14 +22,28 @@ describe('SPV-DASH (forks & re-orgs)', () => {
 
   it('should contain genesis hash', () => {
     chain.getTipHash().should.equal('00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c');
+    chain.getLongestChain().length.should.equal(1);
   });
 
   it('should still contain a branch of 1 when first header is added', () => {
     chain.addHeader(headers[0]);
     chain.getAllBranches().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(2);
+  });
+
+  it('create 1 orphan', () => {
+    chain.addHeader(headers[2]);
+    chain.getOrphans().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(2);
+  });
+
+  it('connect the orphan by adding its parent', () => {
+    chain.addHeader(headers[1]);
+    chain.getOrphans().length.should.equal(0);
+    chain.getAllBranches().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(4);
   });
 });
-
 
 describe('Difficulty Calculation', () => {
   it('should have difficulty of 1 when target is max', () => {
@@ -42,4 +56,3 @@ describe('Difficulty Calculation', () => {
     utils.getDifficulty(testnetMaxTarget).should.be.greaterThan(1);
   });
 });
-
